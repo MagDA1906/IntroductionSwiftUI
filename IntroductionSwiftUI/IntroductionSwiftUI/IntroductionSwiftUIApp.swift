f33dbfb40a296755e4ecb604a5cecc3a3ae8cd41
Denis Magilnitsky <magilnitsky@gmail.com>
2021-09-13 22:23:56 +0300

    first commit

---
+++ IntroductionSwiftUI/IntroductionSwiftUI/IntroductionSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroductionSwiftUIApp.swift
+//  IntroductionSwiftUI
+//
+//  Created by Денис Магильницкий on 13.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct IntroductionSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
